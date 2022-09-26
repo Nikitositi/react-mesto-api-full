9db285e91081107691c4c501990184cfe96ab3ff
Nikita Shcherbakha <nikitositi99@yandex.ru>
2022-09-26 19:03:22 +1000

    настройка cors

--- backend/app.js
+++ backend/app.js
@@ -39,23 +39,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
   const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-
   if (whiteList.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.end();
     return;
   }
-
   next();
 });
 
